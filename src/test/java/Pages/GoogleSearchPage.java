facdf6de9da9ed266fdd6be45d5338d4577695bc
lvuk <luka.vuk456@gmail.com>
2023-02-08 13:23:15 +0100

    Added cross-browser testing feature

--- src/test/java/Pages/GoogleSearchPage.java
+++ src/test/java/Pages/GoogleSearchPage.java
@@ -3,26 +3,37 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
 
 public class GoogleSearchPage {
 
     WebDriver driver;
+    String browser;
 
-    public GoogleSearchPage(WebDriver driver) {
+    public GoogleSearchPage(WebDriver driver, String browser) {
         this.driver = driver;
+        this.browser = browser;
     }
 
     By searchBox = By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input");
-    By searchBtn = By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]");
-    By sauceDemoLink = By.xpath("/html/body/div[7]/div/div[11]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3");
     By acceptGoogleTerms = By.xpath("/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]/div");
     public void Search(String searchInput) {
         driver.findElement(acceptGoogleTerms).click();
         driver.findElement(searchBox).sendKeys(searchInput);
-        driver.findElement(searchBtn).click();
+        driver.findElement(searchBox).sendKeys(Keys.RETURN);
     }
 
     public void AccessWebshop(){
+        By sauceDemoLink;
+
+        switch (this.browser.toLowerCase()){
+            case "chrome":
+                sauceDemoLink = By.xpath(" //*[@id=\"rso\"]/div[1]/div/div/div/div[1]/div/a/h3");
+                break;
+            default:
+                 sauceDemoLink = By.xpath("/html/body/div[7]/div/div[11]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3");
+
+        }
         driver.findElement(sauceDemoLink).click();
     }
 }
--- src/test/java/TestMethods.java
+++ src/test/java/TestMethods.java
@@ -7,62 +7,86 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class TestMethods {
     //-------------------Global Variables-----------------------------------
 //Declare a Webdriver variable
      WebDriver driver;
+     String browser;
     GoogleSearchPage googlePage;
     WebshopPage webshopPage;
     public String URL = "https://www.google.com/";
     //----------------------Test Setup-----------------------------------
     @BeforeClass
+    @Parameters("browser")
+    public void InitDriver(String browser){
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = WebDriverManager.chromedriver().create();
+                this.browser = browser.toLowerCase();
+                break;
+            case "ie":
+                driver = WebDriverManager.iedriver().create();
+                this.browser = browser.toLowerCase();
+                break;
+            case "firefox":
+                driver = WebDriverManager.firefoxdriver().create();
+                this.browser = browser.toLowerCase();
+                break;
+
+            default:
+                driver = null;
+                break;
+        }
+    }
+    @BeforeClass
     public void setupTest() {
     //Create a new WebManagerDriver
-        driver = WebDriverManager.firefoxdriver().create();
         driver.navigate().to(URL);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        googlePage = new GoogleSearchPage(driver);
+        googlePage = new GoogleSearchPage(driver, browser);
         webshopPage = new WebshopPage(driver);
     }
 
     @Test(priority = 0)
+    public void VerifyPageTitle() {
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
+    @Test(priority = 1)
     public void SearchOperation() throws InterruptedException{
 
         googlePage.Search("saucedemo.com");
         Thread.sleep(1000);
         googlePage.AccessWebshop();
     }
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void VerifyLogin() throws InterruptedException {
-        String excpectedURL = "https://www.saucedemo.com/inventory.html";
+        String expectedURL = "https://www.saucedemo.com/inventory.html";
         String actualURL = webshopPage.login();
-        Assert.assertEquals(excpectedURL, actualURL);
+        Assert.assertEquals(expectedURL, actualURL);
 
         Thread.sleep(2000);
     }
-
-    @Test(priority = 2)
+    @Test(priority = 3)
     public void FillTheCart() throws InterruptedException {
         webshopPage.addItemsToCart();
         Thread.sleep(2000);
     }
-
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void RemoveOneItemFromCart() throws InterruptedException{
 
         webshopPage.removeItemFromCart();
         Thread.sleep(2000);
     }
-
-    @Test(priority = 4)
+    @Test(priority = 5)
     public void ProceedWithOrder() throws InterruptedException{
         webshopPage.finishTheOrder("Luka","Vuk", "31000");
-
         Thread.sleep(2000);
     }
+
     //---------------Test TearDown-----------------------------------
     @AfterClass
     public void teardownTest() {
